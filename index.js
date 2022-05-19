48c6767ebbfe23ec2ea44b360cb9c4362b932250
Pedro.sandrini <pedro.sandri@markdigx.com.br>
2022-05-19 10:57:12 -0300

    criando a primeira condição

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ let departureDate = moment(departureDateEntry, "DD/MM/YYYY")
 let today = moment()
 
 let dateDiff = today - departureDate
+
+let choseOption = prompt("Escolha como gostaria de exibir o tempo de partida \n 1- Segundos \n2- Minutos \n3- Horas \n 4- Dias")
+
+if (choseOption ==1 ){
+    let secondsOfDeparture = math.round(dateDiff / 1000)
+    alert("Tempo de vôo" + secondsOfDeparture + "Segundos ")
+}
