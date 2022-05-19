52903937a7c87173c830f4117999b3b53b852d27
Pedro.sandrini <pedro.sandri@markdigx.com.br>
2022-05-19 10:52:10 -0300

    variaveis  com datas atuais

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let departureDateEntry = prompt('Digite a data de partida(formadataDD/MM/YYYY)')
 
 let departureDate = moment(departureDateEntry, "DD/MM/YYYY")
+
+let today = moment()
+
+let dateDiff = today - departureDate
